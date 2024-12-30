41577b26fbc7035090845565e46ef3d85c081023
Kartik Gopal <154347179+kartikgopal01@users.noreply.github.com>
2024-12-30 21:31:40 +0530

    Time: 4 ms (57.02%), Space: 13.1 MB (19.26%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hash_map={}
+        for i , v in enumerate(nums):
+            if target-v in hash_map:
+                return i,hash_map[target-v]
+            else:
+                hash_map[v]=i   
+        
