b493a56aa09f1fbdef241c82a5813691e4c43975
Kartik Gopal <154347179+kartikgopal01@users.noreply.github.com>
2025-07-23 20:58:35 +0530

    Time: 3 ms (21.83%), Space: 46.1 MB (56.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int[] temp=new int[nums.length];
-        int j=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                temp[j]=nums[i];
-                j++;
+        int j = 0; // Pointer to place the next non-zero element
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // Swap current element with the element at index j 
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;   // Move j to the next index for placing non-zero
             }
         }
-        for(int i=j;i<nums.length;i++){
-            temp[i]=0;
-        }
-        for(int i=0;i<nums.length;i++){
-            nums[i]=temp[i];
-        }
-
     }
 }
