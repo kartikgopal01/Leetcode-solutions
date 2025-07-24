b5a8241c70188ffdca4f7c753f7c4e6b29106221
Kartik Gopal <154347179+kartikgopal01@users.noreply.github.com>
2025-07-24 22:01:07 +0530

    Time: 25 ms (67.39%), Space: 43.4 MB (6.2%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,22 +19,21 @@ class Node {
 */
 
 class Solution {
-    Map<Node,Node> visited= new HashMap<>();
+    private HashMap<Node, Node> visited = new HashMap<>();
+
     public Node cloneGraph(Node node) {
-        if(node==null){
-            return node;
-        }
-        if(visited.containsKey(node)){
-           return visited.get(node);
+        if(node == null) return null;
+
+        if(visited.containsKey(node)) {
+            return visited.get(node);
         }
 
-
-        Node cloneNode= new Node(node.val,new ArrayList<>());
-        visited.put(node,cloneNode);
-        for(Node neighbor:node.neighbors)
-        {
-            cloneNode.neighbors.add(cloneGraph(neighbor));
+        Node clone = new Node(node.val, new ArrayList<Node>());
+        visited.put(node, clone);
+        for(Node neighbor: node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
         }
-        return cloneNode;
+
+        return clone;
     }
 }
