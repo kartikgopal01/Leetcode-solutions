0527f914a539110c0544057c013fea8d21daeb97
Kartik Gopal <154347179+kartikgopal01@users.noreply.github.com>
2025-07-24 19:19:21 +0530

    Time: 1 ms (99.83%), Space: 46.1 MB (20.94%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,22 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxReach = 0;
+    int ans=nums.length-1;
+    for(int i=nums.length-2;i>=0;i--){
+           if(nums[i]+i>=ans){
+            ans=i;
+           }
 
-        for (int i = 0; i < nums.length; i++) {
-            // If the current index is beyond the maximum reachable distance, return false
-            if (i > maxReach) {
-                return false;
-            }
-
-            // Update maxReach
-            maxReach = Math.max(maxReach, i + nums[i]);
-
-            // If maxReach reaches or exceeds the last index, return true
-            if (maxReach >= nums.length - 1) {
-                return true;
-            }
         }
-
-        return true;
+        return ans==0?true:false;
     }
 }
