d04dd1d80f16d7ebfbf622b17d0e78e584612c96
Kartik Gopal <154347179+kartikgopal01@users.noreply.github.com>
2025-08-09 18:21:54 +0530

    Time: 1 ms (35.75%), Space: 40.6 MB (96.83%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false; // powers of two are positive
+        return (n & (n - 1)) == 0; // only one bit set
+    }
+}
